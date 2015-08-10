394c63418759fc14102d45d604b124bc3e2001ce
floalex <flo.liang@my.com>
2015-08-10 19:16:40 +0000

    mailing list

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,22 @@
 class VisitorsController < ApplicationController
   
   def new
-    @owner = Owner.new
+    @visitor = Visitor.new
 #   render 'visitors/new'
   end
   
+  def create
+    @visitor = Visitor.new(visitor_params)
+    if @visitor.valid?
+      flash[:notice] = "Signed up #{@visitor.email}."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  private
+  def visitor_params
+    params.require(:visitor).permit(:email)
+  end
 end
